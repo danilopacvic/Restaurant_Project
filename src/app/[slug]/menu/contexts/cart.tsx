897c677a9a2d59e698cc7d14a952c7040eb97549
danilopacvic <202300224@estudantes.ips.pt>
2025-02-23 21:35:38 +0000

    feat: ensure same product can be added more than once to cart

--- src/app/[slug]/menu/contexts/cart.tsx
+++ src/app/[slug]/menu/contexts/cart.tsx
@@ -3,7 +3,7 @@
 import { Product } from "@prisma/client";
 import { createContext, ReactNode, useState } from "react";
 
-interface CartProduct extends Product {
+interface CartProduct extends Pick<Product, "id" | "name" | "price" | "imageUrl"> {
     quantity: number;
 }
 
@@ -11,12 +11,14 @@ export interface ICartContext {
     isOpen: boolean;
     products: CartProduct[];
     toggleCart: () => void;
+    addProduct: (product: CartProduct) => void;
 }
 
 export const CartContext = createContext<ICartContext>({
     isOpen: false,
     products: [],
     toggleCart : () => {},
+    addProduct: () => {},
 });
 
 export const CartProvider = ({children}: {children: ReactNode}) => {
@@ -25,6 +27,25 @@ export const CartProvider = ({children}: {children: ReactNode}) => {
 
     const toggleCart = () => {
         setIsOpen(prev => !prev);
+    };
+
+    const addProduct = (product: CartProduct) => {
+
+        const isOnCart = products.some(prevProducts => prevProducts.id === product.id);
+            if(!isOnCart){
+                return setProducts((prev) => [...prev, product]);
+            }
+            setProducts(prevProducts => {
+                return prevProducts.map(prevProduct => {
+                    if(prevProduct.id === product.id) {
+                        return {
+                            ...prevProduct,
+                            quantity: prevProduct.quantity + product.quantity,
+                        }
+                    }
+                    return prevProduct;
+                });
+            });
     }
 
     return (
@@ -33,6 +54,7 @@ export const CartProvider = ({children}: {children: ReactNode}) => {
             isOpen,
             products,
             toggleCart,
+            addProduct,
     }}>
         {children}
     </CartContext.Provider>)
