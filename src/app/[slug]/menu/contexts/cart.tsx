011d3afa55c1a6809356eff10b9c9561621d74f9
danilopacvic <202300224@estudantes.ips.pt>
2025-02-24 12:55:48 +0000

    feat: remove product from cart

--- src/app/[slug]/menu/contexts/cart.tsx
+++ src/app/[slug]/menu/contexts/cart.tsx
@@ -14,6 +14,7 @@ export interface ICartContext {
     addProduct: (product: CartProduct) => void;
     decreaseProductQuantity: (productId: string) => void;
     increaseProductQuantity: (productId: string) => void;
+    removeProduct: (productId: string) => void;
 }
 
 export const CartContext = createContext<ICartContext>({
@@ -22,7 +23,8 @@ export const CartContext = createContext<ICartContext>({
     toggleCart : () => {},
     addProduct: () => {},
     decreaseProductQuantity: () => {},
-    increaseProductQuantity: () => {}
+    increaseProductQuantity: () => {},
+    removeProduct: () => {}
 });
 
 export const CartProvider = ({children}: {children: ReactNode}) => {
@@ -78,6 +80,9 @@ export const CartProvider = ({children}: {children: ReactNode}) => {
         });
     }
 
+    const removeProduct = (productId: string) => {
+        setProducts(prevProducts => prevProducts.filter(prevProduct => prevProduct.id !== productId));
+    }
     return (
     <CartContext.Provider 
         value={{
@@ -87,6 +92,7 @@ export const CartProvider = ({children}: {children: ReactNode}) => {
             addProduct,
             decreaseProductQuantity,
             increaseProductQuantity,
+            removeProduct,
     }}>
         {children}
     </CartContext.Provider>)
