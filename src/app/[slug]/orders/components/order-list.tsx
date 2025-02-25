322db69b12095130acf33b8dbb2db81965c7e11d
danilopacvic <202300224@estudantes.ips.pt>
2025-02-25 12:58:03 +0000

    feat: add order details

---
+++ src/app/[slug]/orders/components/order-list.tsx
@@ -0,0 +1,80 @@
+import { Button } from "@/components/ui/button";
+import { Card, CardContent } from "@/components/ui/card";
+import { Separator } from "@/components/ui/separator";
+import { formatCurrency } from "@/helpers/format-currency";
+import { OrderStatus, Prisma } from "@prisma/client";
+import { ChevronLeftIcon, ScrollTextIcon } from "lucide-react";
+import Image from "next/image";
+
+interface OrderListProps {
+    orders: Array<
+        Prisma.OrderGetPayload<{
+            include: {
+                restaurant: {
+                    select: {
+                        name: true;
+                        avatarImageUrl: true;
+                    }
+                },
+                orderProducts: {
+                    include: {
+                        product: true;
+                    }
+                }
+            }
+        }>
+    >;
+}
+
+const getStatusLabel = (status: OrderStatus) => {
+    if(status === "FINISHED") return "Finalizado";
+    if(status === "IN_PREPARATION") return "Em Preparo";
+    if(status === "PENDING") return "Pendente";
+    return "";
+}
+
+const OrderList = ({orders}: OrderListProps) => {
+    return (
+        <div className="space-y-6 p-6">
+            <Button size="icon" variant="secondary" className="rounded-full">
+                <ChevronLeftIcon/>
+            </Button>
+            <div className="flex items-center-gap-3">
+                <ScrollTextIcon />
+                <h2 className="text-lg font-semibold">Meus Pedidos</h2>
+            </div>
+            {orders.map(order => (
+                <Card key={order.id}>
+                    <CardContent className="space-y-4 p-5">
+                        <div className={`w-fit rounded-full px-2 py-1 text-xs font-semibold ${order.status === OrderStatus.FINISHED ? "bg-green-500" : "bg-gray-200 text-gray-400"}`}>
+                            {getStatusLabel(order.status)}
+                        </div>
+                        <div className="flex items-center gap-2">
+                            <div className="relative h-5 w-5">
+                                <Image src={order.restaurant.avatarImageUrl} alt={order.restaurant.name} className="rounded-sm" fill/>
+                            </div>
+                            <p className="font-semibold text-sm">
+                                {order.restaurant.name}
+                            </p>
+                        </div>
+                        <Separator />
+                        <div className="space-y-2">
+                        {order.orderProducts.map((orderProduct) => (
+                            <div key={orderProduct.id} className="flex items-center gap-2">
+                                <div className="flex h-5 w-5 items-center justify-center rounded-full bg-gray-400 text-white text-xs font-semibold">
+                                    {orderProduct.quantity}
+                                </div>
+                                <p>{orderProduct.product.name}</p>
+                            </div>
+                        ))}
+                        </div>
+                        <Separator />
+                        <p className="text-sm font-medium">{formatCurrency(order.total)}</p>
+                    </CardContent>
+                </Card>
+            ))}
+        </div>
+    );
+}
+ 
+export default OrderList;
